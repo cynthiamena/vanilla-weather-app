762c3fa1ed7ed207f0e0bedc78d1bc6da9f31a49
cynthiamena <cynthiamena1223@gmail.com>
2023-06-23 18:29:47 -0700

    added humidity and wind to JS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
-let apiKey = "99a398f69eb5ef956faaa6fa2d5a36be"
+function displayTemperature(response){
 
-let apiUrl = `https://api.openweathermap.org/data/3.0/weather?q=New York&appid=${apiKey}`
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
+}
+let apiKey = "99a398f69eb5ef956faaa6fa2d5a36be"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
+
+axios.get(apiUrl).then(displayTemperature);
