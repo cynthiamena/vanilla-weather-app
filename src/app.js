93defb27a33719ee3b23bba1f55f501006ffe87a
cynthiamena <cynthiamena1223@gmail.com>
2023-06-23 20:37:59 -0700

    added icon url,

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ function formatDate(timestamp){
     if (minutes < 10){
         minutes = `0${minutes}`;
     }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let days = [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"];
     let day = days[date.getDay()];
     return`${day} ${hours}:${minutes}`;
 }
@@ -19,6 +26,8 @@ function displayTemperature(response){
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
@@ -26,10 +35,10 @@ function displayTemperature(response){
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-
-
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
-let apiKey = "99a398f69eb5ef956faaa6fa2d5a36be"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
+let apiKey = "99a398f69eb5ef956faaa6fa2d5a36be";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
