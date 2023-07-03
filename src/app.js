21cfae5a8125c72a738d3cbba5fec0f57eba46f8
cynthiamena <cynthiamena1223@gmail.com>
2023-07-02 21:40:16 -0700

    added html into js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,37 @@ function formatDate(timestamp){
     return`${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
 
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function (day){
+        forecastHTML = forecastHTML + 
+        `
+        <div class="col-2">
+            <div class="weather-forecast-date">
+                ${day}
+            </div>
+                <img 
+                    src="http://openweathermap.org/img/wn/01n@2x.png" 
+                    alt="" 
+                    width="42"
+                    />
+            <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">
+                    18º
+                </span>
+                <span class="weather-forecast-temperature-min">
+                    12º
+                </span>
+            </div>
+        </div>
+        `;
+    });
+    forecastHTML = forecastHTML+ `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response){
 
@@ -87,4 +117,5 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
 
+displayForecast();
 
